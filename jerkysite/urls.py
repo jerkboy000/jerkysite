1177295f2c0954d39ed4d3b8dc8eeba56c327357
jerkboy <jerkboy000@gmail.com>
2019-03-03 20:25:54 +0800

    Added urls.py, views.py of app "polls".

--- jerkysite/urls.py
+++ jerkysite/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from hello.views import myView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sayHello/', myView)
+    path('sayHello/', myView),
+    path('polls/', include('polls.urls')),
 ]
